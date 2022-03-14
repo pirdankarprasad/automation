299c0873211804f27f24a2292c57ff8de7a2f767
amoljeure <100512257+amoljeure@users.noreply.github.com>
2022-03-14 19:50:58 +0530

    Revert "Program will added to main"

--- 1234572/Automation/src/pom/Configuration.java
+++
@@ -1,9 +0,0 @@
-package pom;
-
-public class Configuration {
-
-
-    static String chromepath="E:\\Testing\\Chrome driver\\NEW\\chromedriver_win32\\chromedriver.exe";
-	static String screenshot="E:\\Testing\\ScreenShots\\image_";
-}
-
